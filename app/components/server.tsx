bb1fef5e1131f9f40ca58631d0276e4c4b35eb0b
MarcoBofa <marco.bonafini@mail.polimi.it>
2024-11-09 18:52:30 -0600

    cores u

--- app/components/server.tsx
+++ app/components/server.tsx
@@ -483,6 +483,8 @@ const Server: React.FC<ServerProps> = ({
                 className="flex-grow p-2 rounded border-2"
                 id="coresPerProcessor"
               >
+                <option value="2">2</option>
+                <option value="4">4</option>
                 <option value="8">8</option>
                 <option value="12">12</option>
                 <option value="16">16</option>
